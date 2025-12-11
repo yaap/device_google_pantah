9844a1ef50c0941d7f7b37a177f08237df1cdc85
sidharthify <wednisegit@gmail.com>
2025-12-11 08:56:19 +0000

    pantah: Commonize leftover kernel configurations into BoardConfigYAAP
    Also delete unwanted BoardConfigYAAP and merge with BoardConfig
    
    Signed-off-by: sidharthify <wednisegit@gmail.com>

--- BoardConfigYAAP.mk
+++ BoardConfigYAAP.mk
@@ -4,8 +4,21 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
+include device/google/gs201/BoardConfig-common.mk
+
+# Kernel
+BOARD_KERNEL_CMDLINE += swiotlb=noforce
+
 # Kernel modules
+BOARD_BOOTCONFIG += androidboot.load_modules_parallel=true
+
+BOARD_KERNEL_CMDLINE += fips140.load_sequential=1
+BOARD_KERNEL_CMDLINE += exynos_drm.load_sequential=1
+
 BOARD_VENDOR_KERNEL_RAMDISK_KERNEL_MODULES_BLOCKLIST_FILE := $(DEVICE_PATH)/modules/modules.blocklist.vendor_kernel_boot
 BOARD_VENDOR_KERNEL_RAMDISK_KERNEL_MODULES_LOAD_RAW := $(strip $(shell cat $(DEVICE_PATH)/modules/modules.load.vendor_kernel_boot))
 BOARD_VENDOR_KERNEL_RAMDISK_KERNEL_MODULES_LOAD += $(BOARD_VENDOR_KERNEL_RAMDISK_KERNEL_MODULES_LOAD_RAW)
 BOARD_VENDOR_KERNEL_RAMDISK_KERNEL_MODULES += $(addprefix $(KERNEL_MODULE_DIR)/, $(notdir $(BOARD_VENDOR_KERNEL_RAMDISK_KERNEL_MODULES_LOAD_RAW)))
+
+# WiFi
+include $(DEVICE_PATH)/configs/wifi/BoardConfig-wifi.mk
--- panther/BoardConfig.mk
+++ panther/BoardConfig.mk
@@ -14,23 +14,18 @@
 # limitations under the License.
 #
 
-# Enable load module in parallel
-BOARD_BOOTCONFIG += androidboot.load_modules_parallel=true
-
-# The modules which need to be loaded in sequential
-BOARD_KERNEL_CMDLINE += fips140.load_sequential=1
-BOARD_KERNEL_CMDLINE += exynos_drm.load_sequential=1
-
-TARGET_BOARD_INFO_FILE := device/google/pantah/board-info.txt
-TARGET_BOOTLOADER_BOARD_NAME := panther
+TARGET_BOARD_INFO_FILE := $(DEVICE_PATH)/board-info.txt
+TARGET_BOOTLOADER_BOARD_NAME := $(DEVICE_CODENAME)
 TARGET_SCREEN_DENSITY := 420
 
-BOARD_KERNEL_CMDLINE += swiotlb=noforce
-
-include device/google/gs201/BoardConfig-common.mk
 include device/google/pantah/sepolicy/panther-sepolicy.mk
 include device/google/pantah/configs/wifi/BoardConfig-wifi.mk
 
+# SEPolicy
+include device/google/pantah/sepolicy/panther-sepolicy.mk
+
 DEVICE_PATH := device/google/pantah
 VENDOR_PATH := vendor/google/panther
-include $(DEVICE_PATH)/$(TARGET_BOOTLOADER_BOARD_NAME)/BoardConfigYAAP.mk
+
+include $(DEVICE_PATH)/BoardConfigYAAP.mk
+include $(VENDOR_PATH)/BoardConfigVendor.mk
